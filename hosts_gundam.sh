53c67e1d4f6f64c21198a5e067a68c88a950a11d
shiki <shiki@fatework.rock>
2016-03-10 16:02:07 -0500

    renamed:    host_gundam.sh -> hosts_gundam.sh
      milestone: have test on ubuntu to auto fetch hosts repositories.
      TODO: sort -u maybe not work, and also cd $XXX

--- hosts_gundam.sh
+++ hosts_gundam.sh
@@ -4,19 +4,20 @@
 # Copyright (C) 2016 nick All Rights Reserved
 # Maintainer:   nick
 # Created:      2016-01-24
-# LastModify:   2016-02-29
-# Version:      v1.0
+# LastModify:   2016-03-10
+# Version:      v2.0
 # Platform:     windows / macosx/ centos / ubuntu / kali
 # usage:        bash %
 # =============================================================================
 
 
 REPO_NAME_LIST="highsea racaljk liuker0x007"
+CUR_DIR='~/.env_xps'
+
 repo_list=($REPO_NAME_LIST)
 repo_loc_arr=()
 repo_url_arr=()
-
-gen_loc_and_url_arr()
+gen_locdir_and_urlstr_arrays()
 {
     for i in `seq 1 ${#repo_list[@]}`
     do
@@ -33,27 +34,37 @@ gen_loc_and_url_arr()
 #
 # update_hosts_repo loc_dir git_url
 # --------------------------------
-update_hosts_repo()
+update_single_hosts_repo()
 {
     echo -e "\n\033[31m\033[40;32m[+] Now updating the $1 hosts repository... \033[0m"
-    if [ ! -d "$1"_hosts ];
+    if [ ! -d $1 ]
     then
         git clone $2 ./$1
+    else
+        cd ${CUR_DIR}/$1
+        git pull
+        cd $CUR_DIR
     fi
-    cd $1
-    git fetch --all
-    git pull -f
-    cd ..
 }
 
 
-assemble_hosts()
+update_and_assemble_final_hosts()
 {
-    touch _final_hosts_raw
-    touch _final_hosts
+    echo > _final_hosts_raw
+    echo > _final_hosts
+
+    headline=" 
+    # ===========================================================================
+    # Copyright (C) 2016 nick All Rights Reserved
+    # Maintainer:   nick
+    # Created:      2016-01-24
+    # LastModify:   $(date +%F\ %r\ %a)
+    # Version:      v1.0"
+    echo -e "$headline" > _final_hosts_raw
+
     for i in `seq 1 ${#repo_list[@]}` 
     do
-        update_hosts_repo ${repo_loc_arr[$i-1]} ${repo_url_arr[$i-1]}
+        update_single_hosts_repo ${repo_loc_arr[$i-1]} ${repo_url_arr[$i-1]}
         cat ${repo_loc_arr[$i-1]}/hosts >> _final_hosts_raw
     done
     sort -u _final_hosts_raw > _final_hosts
@@ -63,37 +74,54 @@ assemble_hosts()
 #
 # 0x01 Detect os type
 #
-detect_os_type()
+hosts_path=''
+detect_ostype_and_load_hosts()
 {
     ostype=''
-    hosts_path=''
     is_known=1
+    hosts_line_count=0
+
     case "$OSTYPE" in
         linux*) ostype='linux'; hosts_path='/etc/hosts' ;;
         msys*)  ostype='msys' ; hosts_path='/c/Windows/System32/drivers/etc/hosts' ;;
         *)      ostype='unknown'; is_known=0 ;;
     esac
-
-    if $is_known
+    echo $ostype
+    echo $is_known
+    echo $hosts_path
+    if [ $is_known -eq 1 ]
     then
-        echo -e "\n\033[31m\033[40;33m[.] Detect the OS type is $ostyp \033[0m"
+        echo -e "\n\033[31m\033[40;36m[.] Detect the OS type is $ostype \033[0m"
         echo -e "\n\033[31m\033[40;33m[+] Writing the system hosts file... \033[0m"
-        cat _final_hosts > $hosts_path
+
+        user=`whoami`
+        if [ "$user" != "root" ]
+        then
+            su root -c "cat _final_hosts > $hosts_path"
+        fi
     else
-        echo -e "\n\033[31m\033[40;33m[.] Detect the OS type is $ostyp \033[0m"
+        echo -e "\n\033[31m\033[40;33m[-] Unable to detect the OS type! \033[0m"
+        echo -e "\n\033[31m\033[40;33m[-] Will exit and leave hosts gundam! \033[0m"
+    fi
 }
 
 
 # begin the main procedure
 # ========================
+main()
+{
+    echo -e "\n\033[31m\033[40;36m[*] Today is $(date +%F\ %r\ %a)\033[0m"
+    
+    gen_locdir_and_urlstr_arrays
+    update_and_assemble_final_hosts
+    detect_ostype_and_load_hosts
+    
+    echo -e "\n\033[31m\033[40;36m[*] Done! the new hosts file have $(cat $hosts_path | wc -l) lines, Good Luck! :D \033[0m\n"
+    head $hosts_path
+}
 
-echo -e "\n\033[31m\033[40;36m[*] Today is $(date +%F\ %r\ %a)\033[0m"
 
-gen_loc_and_url_arr
-update_
-
-
-
+main
 
 # vim: se ai si et ts=4 sw=4 ft=sh :
 # EOF
